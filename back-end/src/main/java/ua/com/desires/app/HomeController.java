c11d0549804cd9f11448ec53f9ca5c434cea22e1
spikyzero <nikeslavko0906@gmail.com>
2024-07-24 23:48:12 +0300

    Refactored docker initialization.

---
+++ back-end/src/main/java/ua/com/desires/app/HomeController.java
@@ -0,0 +1,14 @@
+package ua.com.desires.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String test() {
+        return "Hello World";
+    }
+
+}
