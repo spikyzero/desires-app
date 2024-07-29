0bc3e14c5549bb131a945d25e8b634463dfdbd56
spikyzero <nikeslavko0906@gmail.com>
2024-07-29 17:57:50 +0300

    Added forms.

---
+++ back-end/src/main/java/ua/com/desires/app/controller/form/UserLoginForm.java
@@ -0,0 +1,20 @@
+package ua.com.desires.app.controller.form;
+
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginForm {
+
+    @Email(message = "Email should be valid")
+    String email;
+    String password;
+
+}
---
+++ back-end/src/main/java/ua/com/desires/app/controller/form/UserRegistrationForm.java
@@ -0,0 +1,24 @@
+package ua.com.desires.app.controller.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationForm {
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    String email;
+    @NotBlank(message = "Name is required")
+    String name;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    String password;
+
+}
