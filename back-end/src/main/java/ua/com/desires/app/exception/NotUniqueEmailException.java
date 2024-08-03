e308536f41e8e6073e8d7d85af99677e8d750013
spikyzero <nikeslavko0906@gmail.com>
2024-08-04 01:40:28 +0300

    Changed exception scope.

--- back-end/src/main/java/ua/com/desires/app/exception/NotUniqueEmailException.java
+++ back-end/src/main/java/ua/com/desires/app/exception/NotUniqueEmailException.java
@@ -3,19 +3,7 @@ package ua.com.desires.app.exception;
 public class NotUniqueEmailException extends RuntimeException {
 
     public NotUniqueEmailException() {
-        super("The email address is already in use.");
-    }
 
-    public NotUniqueEmailException(String message) {
-        super(message);
-    }
-
-    public NotUniqueEmailException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NotUniqueEmailException(Throwable cause) {
-        super(cause);
     }
 
 }
